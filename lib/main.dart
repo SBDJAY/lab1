3300afd8e2a3617d150dddbc54d7ac974b3cd7fa
SBDJAY <sbdjay183@gmail.com>
2026-01-28 11:04:21 -0500

    All Ui with counter on screen and timer button added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 //Wednesday January 28th 2026
 //Flutter Lab1
 
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main () {
@@ -9,7 +10,7 @@ void main () {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key})
+  const MyApp ({super.key});
 
   @override
   Widget build (BuildContext context) {
@@ -24,9 +25,70 @@ class MyApp extends StatelessWidget {
       theme :ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)
-        ),      
-      );
+        ),
+
+        home: const MyHomePage(title: 'Lab N1'),
+        );      
+      
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage ({super.key, required this.title});
 
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+//This Class is here to implement the timer and the counter for the timer 
+class _MyHomePageState extends State<MyHomePage>{
+  int _counter = 0;
+  late Timer timer;
+
+  void _timer() {
+    Timer(const Duration(seconds: 5), handleTimeout);
+  }
+
+  void handleTimeout() {  
+}
+  
+  void _incrementCounter() {
+    setState(() {
+      _counter ++;
+    });
+  }
+
+
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:  Colors.white,
+        title: Text(widget.title),
+      ),
+
+      body : Center (
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Time: '),
+
+            Text('$_counter',
+            style : Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),),
+
+        floatingActionButton: Row(
+          mainAxisAlignment : MainAxisAlignment.center,
+          children: [
+            FloatingActionButton(
+              onPressed: _timer,
+            child: const Icon(Icons.play_circle_outline)),
+          ],
+        ),
+    );
+  }
+}
