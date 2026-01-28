65ce04fc9aad656229314ce2c9451b6d1dca5522
SBDJAY <sbdjay183@gmail.com>
2026-01-28 10:17:08 -0500

    MyApp Class -> Debugger Banner Removed, and Top Bar Changed, and ThemeData Changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,30 @@
 //Flutter Lab1
 
 import 'package:flutter/material.dart';
+
+void main () {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({super.key})
+
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp (
+
+      //Top Bar Title
+      title: 'Lab N1',
+      //Debug banner hide
+      debugShowCheckedModeBanner: false,
+
+      //App Color Style theme
+      theme :ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)
+        ),      
+      );
+  }
+}
+
+
